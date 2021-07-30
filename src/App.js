ff04c177a0747a5651fc3797685deb7f0c7c9a9b
Kartik Setia <kartset10@gmail.com>
2021-07-30 15:09:12 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Photos from './components/photos';
 import React, {Component} from 'react';
 import SearchField from "react-search-field";
+import {Button, Modal, Container, Row, Col} from 'react-bootstrap';
 import {Navbar,Nav} from 'react-bootstrap';
 
 
@@ -17,32 +18,32 @@ export class App extends Component {
   }
 
   //change handler function for search bar query change
-  onChange(v,e) {
+  onChange(e) {
+    console.log(e.target.value)
     this.setState({ 
-      searchText: v 
+      searchText: e.target.value
     });
   } 
 
   render() {
     return (
       <div id="app">
-      <Navbar bg="light" expand="lg" fixed="top" className="App-header center-navbar navbar-dark bg-dark">
-        <Navbar.Brand href="#">Search Photos</Navbar.Brand>
+      <Navbar bg="light" expand="lg" sticky="top" className="center-navbar navbar-dark bg-dark">
           <Nav
             className="mr-auto my-2 my-lg-0"
             style={{ maxHeight: '100px' }}
           >
-            <Nav.Link href="https://github.com/kartset/" >
-              kartset
-            </Nav.Link>
-            <SearchField
-              placeholder="Search..."
-              classNames="searchField"
-              onChange={this.onChange}
-            />
+            <Container>
+              <Row>
+                <Col className="nav justify-content-center"><Navbar.Brand href="#"><h2>Search Photos</h2></Navbar.Brand></Col>
+              </Row>
+              <Row>
+                <Col><input type='text' size="50" className="form-control col-lg-8" type="search" placeholder="Search..." aria-label="Search" className="searchField" onChange={(e) => this.onChange(e)} /></Col>
+              </Row>
+            </Container>
           </Nav>
       </Navbar>
-      <div className="photos">
+      <div >
       <Photos key={this.state.searchText} searchText={this.state.searchText} /> {/*with the use of key it renders everytime it changes so component will mount works for search as well*/}
       {/*<Photos searchText={this.state.searchText} />*/} {/* can work with getDerivedStateFromProps but having trouble re rendering it*/}
       </div> 
